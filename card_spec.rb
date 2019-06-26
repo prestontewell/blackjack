80cf91a905951ea13c16d355c98a5603cac69e94
prestontewell <preston@tewellphoto.com>
2019-06-26 15:06:21 -0500

    initial commit. add card class and built out RSpec testing for class

---
+++ card_spec.rb
@@ -0,0 +1,35 @@
+require_relative 'card'
+
+RSpec.describe Card do
+  before do
+    suit = "Hearts"
+    rank = '8'
+
+    @card = Card.new suit, rank
+  end  
+
+  it 'should respond to suit' do
+    expect(@card).to respond_to(:suit)
+  end
+
+  it 'should respond to rank' do
+    expect(@card).to respond_to(:rank)
+  end
+
+  it 'should respond to show' do
+    expect(@card).to respond_to(:show)
+  end
+
+  it 'should return hearts for suit' do
+    expect(@card.suit).to eq('Hearts')
+  end
+
+  it 'should return 8 for rank' do
+    expect(@card.rank).to eq('8')
+  end
+
+  it 'should return true for show' do
+    expect(@card.show).to eq(true)
+  end
+
+end
