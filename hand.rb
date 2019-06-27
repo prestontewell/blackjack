be9d8cccab85e1dc88a9ec740e0011a873e85394
prestontewell <preston@tewellphoto.com>
2019-06-27 11:01:14 -0500

    add values to Hand class for dealt cards. Add add_card and get_value methods for Hand class

--- hand.rb
+++ hand.rb
@@ -1,12 +1,42 @@
 class Hand
 	attr_accessor :dealt_cards
 
+	VALUES = {
+		'2': 2,
+		'3': 3, 
+		'4': 4, 
+		'5': 5, 
+		'6': 6, 
+		'7': 7, 
+		'8': 8, 
+		'9': 9, 
+		'10': 10, 
+		'Jack': 10, 
+		'Queen': 10, 
+		'King': 10, 
+		'Ace': 1, 
+
+	}
+
 	def initialize
 		@dealt_cards = []
 	end
 
 	def add_card(card)
-		
+		@dealt_cards << card
+	end
+
+	def get_value
+		card_ranks = []
+		result = 0
+		@dealt_cards.each do |card|
+			card_ranks << card.rank
+		end
+		card_ranks.each do |rank|
+			rank = rank.to_sym
+			result += VALUES[rank]
+		end
+		return result
 	end
 
 end
--- hand_spec.rb
+++ hand_spec.rb
@@ -17,15 +17,35 @@ RSpec.describe Hand do
 			expect(@hand).to respond_to(:add_card)
 		end
 
-		it 'should return correct number of cards added' do
+		it 'should return correct properties of cards added' do
+			card1 = Card.new('Hearts', '8')
+			card2 = Card.new('Clubs', '7')
+
+			@hand.add_card(card1)
+			@hand.add_card(card2)
+			expect(@hand.dealt_cards.size).to eq(2)
+
+			expect(@hand.dealt_cards.first.suit).to eq('Hearts')
+			expect(@hand.dealt_cards.last.suit).to eq('Clubs')
+			expect(@hand.dealt_cards.first.rank).to eq('8')
+			expect(@hand.dealt_cards.last.rank).to eq('7')
+
 		end
 	end
 
 	describe '#get_value' do
 		it 'responds to get_value' do
+			expect(@hand).to respond_to(:get_value)
 		end
 
 		it 'returns correct value with no ace' do
+			card1 = Card.new('Hearts', '8')
+			card2 = Card.new('Clubs', '7')
+
+			@hand.add_card(card1)
+			@hand.add_card(card2)
+			expect(@hand.get_value).to eq(15)
+
 		end
 
 		it 'returns correct value with ace and jack' do
